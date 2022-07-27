2cb74130156d401aa96d98bdf80a7f126f48da50
yothebob <b.cmoderndesigns@gmail.com>
2022-07-27 07:17:34 -0700

    start building it with python first

---
+++ bf.py
@@ -0,0 +1,69 @@
+
+class LinkedList:
+    def __init__(head=False, next=None, prev=None):
+        self.head = head
+        self.next = next
+        self.prev = prev
+        
+class Interpreter:
+
+    def __init__(self):
+        self.pointers = [0]
+        self.pointer_index = 0
+        self.code = input("test code:")
+    
+    def move_left(self):
+        if self.pointer_index != 0:
+            self.pointer_index -= 1
+        else:
+            print("bad pointer call, waiting for linked list support")
+            self.pointer_index = -1
+
+    def move_right(self):
+        if self.pointer_index == (len(self.pointers) -1):
+            self.pointers.append(0)
+            self.pointer_index += 1
+        else:
+            self.pointer_index += 1
+
+    def add_one(self):
+        self.pointers[self.pointer_index] += 1
+        
+    def sub_one(self):
+        self.pointers[self.pointer_index] -= 1
+
+    def run_loop(self,loop_code):
+        while True:
+            if self.pointers[self.pointer_index] == 1:
+                return 
+            else:
+                self.enterpret(loop_code)
+            
+    def enterpret(self, code):
+        for index in range(len(code)):
+            if code[index] == ">":
+                self.move_right()
+            elif code[index] == "<":
+                self.move_left()
+            elif code[index] == "+":
+                self.add_one()
+            elif code[index] == "-":
+                self.sub_one()
+            elif code[index] == "]":
+                break
+            elif code[index] == "[":
+                self.run_loop(code[(index+1):])
+            elif code[index] == ",":
+                pass # get input
+            elif code[index] == ".":
+                pass # print ascii char
+        print(self.pointers)
+
+    def run(self):
+        self.enterpret(self.code)
+
+
+
+ii = Interpreter()
+ii.run()
+print(ii.pointers)
