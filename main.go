7457dc1bb28b1db4d6c11ac50bc62038193094a9
Brandon <b.cmoderndesigns@gmail.com>
2022-08-16 20:56:08 -0700

    go was working, now I am just tearing it appart go get it working again

--- main.go
+++ main.go
@@ -1,71 +1,64 @@
 package main
 
 import (
-  "fmt"
+	"strconv"
+	"fmt"
 )
 
-type LinkedList struct {
-	headNode int
-	nextNode *LinkedList
-	prevNode *LinkedList
-	value int
-}
-
-func moveLeft(pi *LinkedList) {
-	if pi.headNode == 1{
-		pi.headNode = 0
-		newHead := new(LinkedList)
-		newHead.headNode = 1
-		newHead.nextNode = pi
-		return &newHead
-	} else {
-		return &pi.prevNode
-	}
-}
-
-func moveRight(pi *LinkedList) {
-	if pi.nextNode {
-		newNode := new(LinkedList)
-		pi.nextNode = newNode
-		newNode.prevNode = pi
-		return &newNode
-	} else {
-		return &pi.nextNode
-	}
-}
-
-func addOne (pi *LinkedList) {
-	return  pi.value + 1
-}
-
-func subOne (pi *LinkedList) {
-	return  pi.value - 1 
-}
-
-
-func interpret(code string, pointer *LinkedList){
+func interpret(code string, list []int, pointer int) {
 	for i:=0; i < len(code);i++ {
-		fmt.Println(i) // index
-		fmt.Println(string(code[i])) // char
-		fmt.Println(code[i]) //ascii
-		if string(code[i]) == ">" {
-			move_right(pointer)
-		}
-		if string(code[i]) == "<" {
-			move_left(pointer)
-		}
-		if string(code[i]) == "[" {
-		}
-		if string(code[i]) == "]" {
-			return 
-		}
-		if string(code[i]) == "+" {
-		}
-		if string(code[i]) == "-" {
-		}
-		if string(code[i]) == "." {
-		}
-		if string(code[i]) == "," {
+
+		switch string(code[i]) {
+			
+		case "+":
+			//plus 1
+			list[pointer] = list[pointer] + 1
+			
+		case "-":
+			//minus 1
+			list[pointer] = list[pointer] - 1
+			
+		case ">":
+			// shift right
+			if pointer > len(list) {
+				pointer = pointer + 1
+			} else {
+				pointer = pointer + 1
+				list = append(list, 0)
+			}
+
+		case "<":
+			// shift left
+			if pointer > 0 {
+				pointer = pointer - 1
+			} else {
+				pointer = pointer - 1
+				list = append([]int{0}, list...)
+			}
+
+		case "[":
+			//startloop
+			fmt.Println(string(code[i]))
+
+		case "]":
+			//endloop
+			fmt.Println(string(code[i]))
+
+		case ".":
+			//show ascii
+			fmt.Println(string(code[i]))
+
+		case ",":
+			// get_input
+			var input string
+			fmt.Scanln(&input)
+			if i, err := strconv.Atoi(input); err == nil {
+				list[pointer] = list[pointer] + int(i)
+			} else {
+				// add ascii value
+				runes := []rune(input)
+				list[pointer] = list[pointer] + int(runes[0])
+			}
 		}
 	}
 }
@@ -73,8 +66,10 @@ func interpret(code string, pointer *LinkedList){
 func main(){
 	fmt.Println("Type Code:")
 	var bf_code string
-	pointer := new(LinkedList)
+	pointer := 0
+	list := []int{0}
 	fmt.Scanln(&bf_code)
-	interpret(bf_code)
+	interpret(bf_code, list, pointer)
+	fmt.Println(list)
 }
 
